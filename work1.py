cf01e7e6489fed3899a6b27b9e7040fa080b1c86
ABOLFAZL <naabolfazl820@gmail.com>
2026-08-15 20:33:35 +0330

    work

---
+++ work1.py
@@ -0,0 +1,10 @@
+nume1=float(input("عدد اول را وارد کنید :"))
+nume2=float(input("عدد دوم را وارد کنید :"))
+mian= nume1 + nume2
+moad= mian / 2
+masa= nume1 * nume2
+remainder= nume1 % nume2
+print(mian)
+print(moad)
+print(masa)
+print(remainder)
